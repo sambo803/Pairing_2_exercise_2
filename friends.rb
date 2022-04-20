9ca02f25744cb1082240ad6f94bf0f59a29db236
Samuel Bacon <sbacon803@gmail.com>
2022-04-20 15:18:51 -0600

    Add buddy cat

--- friends.rb
+++ friends.rb
@@ -1,5 +1,9 @@
-bff = "Dog"
-if bff == "Dog"
-  puts "You're so lucky to have such a loyal friend!"
+best_friend = "Dog"
+buddy = "Cat"
+  best_friend = false
+  buddy = true
+if best_friend == true
+  puts "You're so lucky to have a dog in your life!"
+elsif buddy == true
+  puts "Your cat is okay too, I guess."
 end
- 
